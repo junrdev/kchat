036898ff3458b856da4972023e00cfbe38883cf6
star <juniorprogrammer09@gmail.com>
2023-06-05 15:48:15 +0000

    experimental

--- app/src/main/java/com/junrdev/kchat/fragments/chatlist.java
+++ app/src/main/java/com/junrdev/kchat/fragments/chatlist.java
@@ -3,12 +3,15 @@ package com.junrdev.kchat.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.junrdev.kchat.R;
+import com.junrdev.kchat.databinding.FragmentChatlistBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,10 +60,22 @@ public class chatlist extends Fragment {
         }
     }
 
+    private FragmentChatlistBinding binding;
+
+    private FirebaseAuth auth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatlist, container, false);
+
+        binding = FragmentChatlistBinding.inflate(inflater);
+        auth = FirebaseAuth.getInstance();
+
+        binding.LogoutButton.setOnClickListener(v ->{
+            auth.signOut();
+            Navigation.findNavController(requireView()).popBackStack();
+        });
+
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/junrdev/kchat/fragments/loginactivity.java
+++ app/src/main/java/com/junrdev/kchat/fragments/loginactivity.java
@@ -1,14 +1,27 @@
 package com.junrdev.kchat.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.junrdev.kchat.R;
 import com.junrdev.kchat.databinding.FragmentLoginactivityBinding;
 
@@ -30,6 +43,8 @@ public class loginactivity extends Fragment {
 
 
     private FragmentLoginactivityBinding binding;
+
+    private LinearLayout loginLoading;
 
     public loginactivity() {
         // Required empty public constructor
@@ -62,15 +77,74 @@ public class loginactivity extends Fragment {
         }
     }
 
+
+    private EditText email, password;
+
+    private FirebaseAuth firebaseAuth;
+
+    private FirebaseUser currentUser;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentLoginactivityBinding.inflate(inflater);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        email = binding.loginEmail;
+        password = binding.loginPassword;
+        loginLoading = binding.loadinglayout.loginLoading;
+
+        loginLoading.setVisibility(View.GONE);
+
+        if (currentUser != null) {
+            loadHome();
+        }
+
+        binding.loginButton.setOnClickListener(v -> {
+
+            if (TextUtils.isEmpty(email.getText()) || TextUtils.isEmpty(password.getText()))
+                Toast.makeText(binding.getRoot().getContext(), "Provide all fields", Toast.LENGTH_SHORT).show();
+            else {
+
+                loginLoading.setVisibility(View.VISIBLE);
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            currentUser = task.getResult().getUser();
+                            loginLoading.setVisibility(View.GONE);
+                            Navigation.findNavController(requireView()).navigate(R.id.action_loginactivity_to_chatlist);
+                        }
+                    }
+                });
+            }
+
+        });
 
         binding.signUpPrompt.setOnClickListener(v ->
                 Navigation.findNavController(requireView()).navigate(R.id.action_loginactivity_to_signupactivity)
         );
 
         return binding.getRoot();
+    }
+
+    private void loadHome() {
+        Navigation.findNavController(requireView()).navigate(R.id.action_loginactivity_to_chatlist);
+
+        assert  getActivity() != null;
+        getActivity().finish();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("TAG", "onResume: ");
+        if (currentUser != null) {
+            assert getActivity() != null;
+            getActivity().finish();
+        }
     }
 }
